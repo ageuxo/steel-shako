90b7537fba6b936278acaf6ea24d95950dfbb3a3
ageuxo <ageuxo@gmail.com>
2025-09-20 14:05:53 +0200

    Add damage type datagen Add mining ray damage type

--- src/main/java/org/ageuxo/steelshako/data/DataProviders.java
+++ src/main/java/org/ageuxo/steelshako/data/DataProviders.java
@@ -1,12 +1,18 @@
 package org.ageuxo.steelshako.data;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import org.ageuxo.steelshako.ModDamageTypes;
 import org.ageuxo.steelshako.SteelShakoMod;
 
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class DataProviders {
@@ -29,5 +35,15 @@ public class DataProviders {
                 event.includeServer(), new ModItemModelProvider(output, fileHelper)
         );
 
+        // DataPack object provider
+        generator.addProvider(
+                event.includeServer(),
+                (DataProvider.Factory<DatapackBuiltinEntriesProvider>) out -> new DatapackBuiltinEntriesProvider(out, lookupProvider, new RegistrySetBuilder()
+                        .add(Registries.DAMAGE_TYPE, ModDamageTypes::addDamageTypes),
+                        Set.of(SteelShakoMod.MOD_ID)
+                )
+        );
     }
+
+
 }
