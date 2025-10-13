06809cff65bc6f5de954a303e6b7a5a6c696ae2e
ageuxo <ageuxo@gmail.com>
2025-10-13 16:35:30 +0200

    Change Automaton to only target entities in a tag

--- src/main/java/org/ageuxo/steelshako/ModTags.java
+++ src/main/java/org/ageuxo/steelshako/ModTags.java
@@ -2,9 +2,12 @@ package org.ageuxo.steelshako;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 
 public class ModTags {
 
     public static final TagKey<Item> LASER_AMMO = TagKey.create(Registries.ITEM, SteelShakoMod.modRL("laser_ammo"));
+
+    public static final TagKey<EntityType<?>> AUTOMATON_MOB_TARGETS = TagKey.create(Registries.ENTITY_TYPE, SteelShakoMod.modRL("automaton_mob_targets"));
 }
